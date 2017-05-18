9f9b258d2bc11adc17ead3917b81896eddf90794
Stefan Wager <swager@stanford.edu>
2017-05-17 21:52:25 -0400

    make new plots

--- examples/experiment.R
+++ examples/experiment.R
@@ -1,10 +1,11 @@
 rm(list = ls())
 
-library(foreign)
+setwd("~/git_local/bounds/examples/")
 
-detach("package:bounds", unload=TRUE)
 library(bounds)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(foreign)
+library(zoo) # for plotting only
+library(splines) #for plotting only...
 
 d = read.dta("minorities/all08_11_indigenous.dta")
 nrow(d)
@@ -27,20 +28,33 @@ hist(y, breaks=21, col="grey")
 cat("Number units:", n, "\n")
 
 ##############################################
-# Test code
+# Run methods
 ##############################################
 
 out.al.9 = bounds(y, sampling.ratio = 9, constraint="none")
 out.lc.9 = bounds(y, sampling.ratio = 9, constraint="logconcave")
 out.sy.9 = bounds(y, sampling.ratio = 9, constraint="symmetric")
 
-# out.al.5 = bounds(y, sampling.ratio = 5, constraint="none")
-# out.lc.5 = bounds(y, sampling.ratio = 5, constraint="logconcave")
-# out.sy.5 = bounds(y, sampling.ratio = 5, constraint="symmetric")
-# 
-# out.al.2 = bounds(y, sampling.ratio = 2, constraint="none")
-# out.lc.2 = bounds(y, sampling.ratio = 2, constraint="logconcave")
-# out.sy.2 = bounds(y, sampling.ratio = 2, constraint="symmetric")
+
+
+##############################################
+# Make plots
+##############################################
+
+df = 7
+
+zeropad = function(x) c(rep(0, 100), x, rep(0, 100))
+unpad = function(x) (x[101:(length(x) - 100)])
+
+my.na.approx = function(xxx) { 
+  aaa = approx((1:length(xxx))[!is.na(xxx)], xxx[!is.na(xxx)], 1:length(xxx))$y
+  max.idx = max(which(!is.na(aaa)))
+  aaa[1:length(xxx) > max.idx] = aaa[max.idx]
+  min.idx = min(which(!is.na(aaa)))
+  aaa[1:length(xxx) < min.idx] = aaa[min.idx]
+  aaa
+}
+
 
 xvals.all = out.al.9$upper.bound.internal$raw$xvals
 good.idx = which(xvals.all >= min(y) & xvals.all <= max(y))
@@ -58,19 +72,7 @@ fhat.al = (Fhat.al - c(0, Fhat.al[-K])) / (xvals[2] - xvals[1])
 fhat.lc = (Fhat.lc - c(0, Fhat.lc[-K])) / (xvals[2] - xvals[1])
 fhat.sy = (Fhat.sy - c(0, Fhat.sy[-K])) / (xvals[2] - xvals[1])
 
-df = 9
 xhat.smooth = unpad(predict(glm(zeropad(xhat) ~ ns(1:(length(xhat) + 200), df = df), family=quasi(link = "log", variance = "mu")), type = "response"))
-
-#fhat.lc.ratio = smooth.spline(na.approx(fhat.lc / fhat), df = df)$y
-
-my.na.approx = function(xxx) { 
-  aaa = approx((1:length(xxx))[!is.na(xxx)], xxx[!is.na(xxx)], 1:length(xxx))$y
-  max.idx = max(which(!is.na(aaa)))
-  aaa[1:length(xxx) > max.idx] = aaa[max.idx]
-  min.idx = min(which(!is.na(aaa)))
-  aaa[1:length(xxx) < min.idx] = aaa[min.idx]
-  aaa
-}
 
 fhat.al.ratio = my.na.approx(fhat.al / xhat)
 fhat.lc.ratio = smooth.spline(my.na.approx(fhat.lc / xhat), df = df)$y
@@ -79,9 +81,21 @@ fhat.sy.ratio = smooth.spline(my.na.approx(fhat.sy / xhat), df = df)$y
 rng = range(xhat.smooth, xhat.smooth * fhat.al.ratio, xhat.smooth * fhat.lc.ratio, xhat.smooth * fhat.sy.ratio, na.rm = TRUE)
 
 pdf("minorities_DEN.pdf")
+pardef = par(mar = c(5, 4, 4, 2) + 0.5, cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 plot(xvals, xhat.smooth, lwd = 2, ylim = rng, xlab="y", ylab="f(y)", type = "l")
 lines(xvals, xhat.smooth * fhat.lc.ratio, col = 2, lwd = 2)
 lines(xvals, xhat.smooth * fhat.al.ratio, col = 4, lwd = 2)
-lines(xvals, xhat.smooth * fhat.sy.ratio, col = 5, lwd = 2)
-legend("topleft", c("Uncorrected", "Symmetry const.", "Log-concave constr.", "Plain Aronow-Lee"), lwd = 2, col = c(1, 5, 2, 4))
+lines(xvals, xhat.smooth * fhat.sy.ratio, col = 3, lwd = 2)
+legend("topleft", c("Uncorrected", "Symmetric", "Log-concave", "No Constraint"), lwd = 2, col = c(1, 3, 2, 4), cex = 1.5)
+par = pardef
+dev.off()
+
+pdf("minorities_CDF.pdf")
+pardef = par(mar = c(5, 4, 4, 2) + 0.5, cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+plot(xvals, Xhat, lwd = 2, xlab="y", ylab="F(y)", type = "l")
+lines(xvals, Fhat.lc, col = 2, lwd = 2)
+lines(xvals, Fhat.al, col = 4, lwd = 2)
+lines(xvals, Fhat.sy, col = 3, lwd = 2)
+legend("topleft", c("Uncorrected", "Symmetric", "Log-concave", "No Constraint"), lwd = 2, col = c(1, 3, 2, 4), cex=1.5)
+par = pardef
 dev.off()
